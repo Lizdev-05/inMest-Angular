fc27761a7030e67805e6f82ac94978688357664d
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2023-11-22 10:50:11 +0000

    Create side bar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './main_features/dashboard/dashboard.component';
+import { MyClassesComponent } from './main_features/my-classes/my-classes.component';
+import { UsersComponent } from './main_features/users/users.component';
+import { AttendanceComponent } from './main_features/attendance/attendance.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: DashboardComponent,
+  },
+  {
+    path: 'my-classes',
+    component: MyClassesComponent,
+  },
+  {
+    path: 'users',
+    component: UsersComponent,
+  },
+  {
+    path: 'attendance',
+    component: AttendanceComponent,
+  },
+];
