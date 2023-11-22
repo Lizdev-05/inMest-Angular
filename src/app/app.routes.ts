a6485578a4ed615e12e7a9cded0e7a5712620c51
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2023-11-22 11:03:50 +0000

    Use router to route the navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,9 @@ import { DashboardComponent } from './main_features/dashboard/dashboard.componen
 import { MyClassesComponent } from './main_features/my-classes/my-classes.component';
 import { UsersComponent } from './main_features/users/users.component';
 import { AttendanceComponent } from './main_features/attendance/attendance.component';
+import { LeaveComponent } from './main_features/leave/leave.component';
+import { RequestComponent } from './main_features/request/request.component';
+import { AnalyticsComponent } from './main_features/analytics/analytics.component';
 
 export const routes: Routes = [
   {
@@ -20,5 +23,17 @@ export const routes: Routes = [
   {
     path: 'attendance',
     component: AttendanceComponent,
+  },
+  {
+    path: 'leave',
+    component: LeaveComponent,
+  },
+  {
+    path: 'request',
+    component: RequestComponent,
+  },
+  {
+    path: 'analytics',
+    component: AnalyticsComponent,
   },
 ];
